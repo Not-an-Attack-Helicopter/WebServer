b30aa8a07375c88bc5d0073eac930312fd5f150c
benstor214 <benstor214@posteo.net>
2026-08-29 23:12:38 +0200

    Order 66

--- includes/parse_helper.hpp
+++
@@ -1,9 +0,0 @@
-#include "webserver.hpp"
-#include <stdexcept>
-
-LocationConfig  parse_location_block(const std::vector<std::string>& tokens, size_t& i);
-ServerConfig    parse_server_block(const std::vector<std::string>& tokens, size_t& i, const Config& config);
-Config          parse_config_file(const std::string& config_file);
-bool            valid_CGI(const std::string& path);
-bool            valid_CGI_ext(const std::string& ext);
-bool            valid_conf_ext(const std::string& filename);
